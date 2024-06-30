3b235e68f3558c621dc07ed3d270ad2804023c09
GYoung <ytok1108@aol.com>
2024-06-30 19:11:35 +0900

    player 만들기

---
+++ 001_Bevy_Tutorial/03_affanshahid_learning_game_dev_bevy/a06_color_player/src/main.rs
@@ -0,0 +1,100 @@
+use bevy::{
+    app::{App, Startup},
+    asset::Assets,
+    color::Color,
+    ecs::system::{Commands, ResMut},
+    math::Vec3,
+    prelude::{default, Camera2dBundle, Circle, ClearColor, ColorMaterial, PluginGroup},
+    render::mesh::Mesh,
+    sprite::{MaterialMesh2dBundle, Mesh2dHandle, Sprite, SpriteBundle},
+    transform::components::Transform,
+    window::{Window, WindowPlugin, WindowResolution},
+    DefaultPlugins,
+};
+
+const WINDOW_WIDTH: f32 = 1024.0;
+const WINDOW_HEIGHT: f32 = 720.0;
+
+const WINDOW_BOTTOM_Y: f32 = WINDOW_HEIGHT / -2.0;
+const WINDOW_LEFT_X: f32 = WINDOW_WIDTH / -2.0;
+
+const COLOR_BACKGROUND: Color = Color::srgba(0.29, 0.31, 0.41, 1.0);
+const COLOR_PLATFORM: Color = Color::srgba(0.13, 0.13, 0.23, 1.0);
+const COLOR_PLAYER: Color = Color::srgba(0.60, 0.55, 0.60, 1.0);
+
+const LIME_GREEN_COLOR: Color = Color::srgba(0.19608, 0.80392, 0.01961, 1.0);
+const AQUA_COLOR: Color = Color::hsl(180.0, 1.00, 0.5);
+const INDIAN_RED: Color = Color::srgba(0.80392, 0.36078, 0.36078, 1.0);
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: LIME_GREEN_COLOR,
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(-100.0, WINDOW_BOTTOM_Y + (200.0 / 2.0), 0.0),
+            scale: Vec3::new(75.0, 200.0, 1.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: AQUA_COLOR,
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(100.0, WINDOW_BOTTOM_Y + (350.0 / 2.0), 0.0),
+            scale: Vec3::new(50.0, 350.0, 1.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: INDIAN_RED,
+            ..Default::default()
+        },
+        transform: Transform {
+            translation: Vec3::new(350.0, WINDOW_BOTTOM_Y + (250.0 / 2.0), 0.0),
+            scale: Vec3::new(150.0, 250.0, 1.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
+
+    // let meshes = Circle::new(0.0);
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(Circle::default()).into(),
+        material: materials.add(ColorMaterial::from(COLOR_PLAYER)),
+        transform: Transform {
+            translation: Vec3::new(WINDOW_LEFT_X + 100.0, WINDOW_BOTTOM_Y + 30.0, 0.0),
+            scale: Vec3::new(30.0, 30.0, 1.0),
+            ..Default::default()
+        },
+        ..default()
+    });
+    commands.spawn(Camera2dBundle::default());
+}
+fn main() {
+    App::new()
+        .insert_resource(ClearColor(COLOR_BACKGROUND)) // resource added
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "Bevy Platformer".to_string(),
+                resolution: WindowResolution::new(WINDOW_WIDTH, WINDOW_HEIGHT),
+                resizable: false,
+                ..Default::default()
+            }),
+            ..Default::default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
