8a1280fb859dadc8f3a120013af4476c519e9c74
GYoung <ytok1108@aol.com>
2024-06-30 21:45:16 +0900

    키보드 입력

---
+++ 001_Bevy_Tutorial/03_affanshahid_learning_game_dev_bevy/Day2_Rapier_rs/a05_moving_player/src/platforms.rs
@@ -0,0 +1,37 @@
+use bevy::{
+    ecs::bundle::Bundle,
+    math::Vec3,
+    sprite::{Sprite, SpriteBundle},
+    transform::components::Transform,
+};
+use bevy_rapier2d::prelude::{Collider, RigidBody};
+
+use crate::{COLOR_PLATFORM, WINDOW_BOTTOM_Y};
+
+#[derive(Bundle)]
+pub struct PlatformBundle {
+    sprite_bundle: SpriteBundle,
+    body: RigidBody,
+    collider: Collider,
+}
+
+impl PlatformBundle {
+    pub fn new(x: f32, scale: Vec3) -> Self {
+        Self {
+            sprite_bundle: SpriteBundle {
+                sprite: Sprite {
+                    color: COLOR_PLATFORM,
+                    ..Default::default()
+                },
+                transform: Transform {
+                    translation: Vec3::new(x, WINDOW_BOTTOM_Y + (scale.y / 2.0), 0.0),
+                    scale,
+                    ..Default::default()
+                },
+                ..Default::default()
+            },
+            body: RigidBody::Fixed,
+            collider: Collider::cuboid(0.5, 0.5),
+        }
+    }
+}
