222b4c3f6660a4bc53c08aae09a3e3b3f79d563c
GYoung <ytok1108@aol.com>
2024-06-30 13:26:46 +0900

    바닥에 그림 붙히기

--- 001_Bevy_Tutorial/03_affanshahid_learning_game_dev_bevy/a04_redering_3_mesh/src/main.rs
+++ 001_Bevy_Tutorial/03_affanshahid_learning_game_dev_bevy/a04_redering_3_mesh/src/main.rs
@@ -59,11 +59,6 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 }
 fn main() {
-    // App::new()
-    //     .add_plugins(DefaultPlugins)
-    //     .add_systems(Startup, setup)
-    //     .run();
-
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
