c7465b5fbaacdf884cf367aa4c2041a5895a5cf5
GYoung <ytok1108@aol.com>
2024-07-11 20:49:51 +0900

    bevy 0.14 && avian2d  avian3d

--- 001_Bevy_Tutorial/06_avian2d_3d_xpbd/001_3d/a02_demo_3d/src/main.rs
+++ 001_Bevy_Tutorial/06_avian2d_3d_xpbd/001_3d/a02_demo_3d/src/main.rs
@@ -1,13 +1,25 @@
-use avian3d::prelude::*;
-use bevy::prelude::*;
+use avian3d::{
+    collision::Collider,
+    dynamics::rigid_body::{AngularVelocity, RigidBody},
+    PhysicsPlugins,
+};
 
-fn main() {
-    App::new()
-        // Enable physics
-        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
-        .add_systems(Startup, setup)
-        .run();
-}
+use bevy::{
+    app::{App, Startup},
+    asset::Assets,
+    color::Color,
+    core_pipeline::core_3d::Camera3dBundle,
+    ecs::system::{Commands, ResMut},
+    math::{
+        primitives::{Cuboid, Cylinder},
+        Dir3, Vec3,
+    },
+    pbr::{PbrBundle, PointLight, PointLightBundle, StandardMaterial},
+    prelude::default,
+    render::mesh::Mesh,
+    transform::components::Transform,
+    DefaultPlugins,
+};
 
 fn setup(
     mut commands: Commands,
@@ -53,4 +65,12 @@ fn setup(
         transform: Transform::from_xyz(-2.5, 4.5, 9.0).looking_at(Vec3::ZERO, Dir3::Y),
         ..default()
     });
+}
+
+fn main() {
+    App::new()
+        // Enable physics
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_systems(Startup, setup)
+        .run();
 }
